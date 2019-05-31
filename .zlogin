e6fbf9ae37aa98720f4c0182b7ddee210d8239d0
sharky <sharky@schaack.io>
2019-05-31 11:07:58 +0200

    install.sh

---
+++ .zlogin
@@ -0,0 +1,4 @@
+
+PATH=/usr/local/bin:$PATH
+PATH=/usr/local/bin:/usr/local/sbin:$PATH
+
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+dotfiles=$HOME/.dotfiles
+zsh_folder=$dotfiles/.zsh
+zshrc=$dotfiles/.zshrc
+vimrc=$dotfiles/.vimrc
+vim_folder=$dotfiles/.vim
+tmux_conf=$dotfiles/.tmux.conf
+gitignore=$dotfiles/.gitignore
+

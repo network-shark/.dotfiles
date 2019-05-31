382f9563eea38b3480d9f984b303c9a2d1c8b825
sharky <sharky@schaack.io>
2019-05-31 11:28:32 +0200

    install.sh update again

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ else
 fi
 
 
-if [ ! -e $fzf_dir ]; then
+if [ ! -d $fzf_dir ]; then
   echo "Installing FZF"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   yes | ~/.fzf/install
@@ -33,7 +33,7 @@ else
 echo "Creating Symlinks in $Home environment"
 
 ## ZSH
-[[ ! -e $HOME/.zsh ]] && ln -s $zsh_dir $HOME/.zsh \
+[[ ! -d $HOME/.zsh ]] && ln -s $zsh_dir $HOME/.zsh \
   || echo ".zsh already exists..."
 
 [[ ! -e $HOME/.zshrc ]] && ln -s $zshrc $HOME/.zshrc \
@@ -43,7 +43,7 @@ echo "Creating Symlinks in $Home environment"
   || echo ".zlogin already exists..."
 
 ## VIM
-[[ ! -e $HOME/.vim ]] && ln -s $vim_dir $HOME/.zsh \
+[[ ! -d $HOME/.vim ]] && ln -s $vim_dir $HOME/.zsh \
   || echo ".vim already exists..."
 
 [[ ! -e $HOME/.vimrc ]] && ln -s $vimrc $HOME/.vimrc \
@@ -52,6 +52,4 @@ echo "Creating Symlinks in $Home environment"
 ## TMUX
 [[ ! -e $HOME/.tmux.conf ]] && ln -s $tmux_conf $HOME/.tmux.conf \
   || echo ".tmux.conf already exists..."
-
-
 

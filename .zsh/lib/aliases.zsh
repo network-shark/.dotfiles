af59be5bf6a6e5ce77f84299decae70624c71546
Sebastian Schaack <sharky@schaack.io>
2021-04-01 23:12:09 +0200

    created lazygit func

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -3,6 +3,12 @@ alias lh='ls -lah'
 alias ...='cd ../..'
 alias now='date +%s'
 alias dotfiles='/usr/local/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+function lazygit() {
+    git add .
+    git commit -a -m "$1"
+    git push
+}
+
 
 
 alias a='fasd -a'        # any

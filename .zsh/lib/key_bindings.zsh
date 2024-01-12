1f443014d5ada1094cacd0a7530f7d2131505130
Sharky <sharky@schaack.io>
2024-01-12 19:19:13 +0100

    add some comments

--- .zsh/lib/key_bindings.zsh
+++ .zsh/lib/key_bindings.zsh
@@ -5,8 +5,6 @@ bindkey '\ew' kill-region
 bindkey -s '\el' "ls\n"
 bindkey -s '\e.' "..\n"
 bindkey '^r' history-incremental-search-backward
-bindkey "^[[5~" up-line-or-history
-bindkey "^[[6~" down-line-or-history
 
 # make search up and down work, so partially type and hit up/down to find relevant stuff
 bindkey '^[[A' up-line-or-search
@@ -20,9 +18,8 @@ bindkey "^[[4~" end-of-line
 bindkey "^[OF" end-of-line
 bindkey ' ' magic-space    # also do history expansion on space
 
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
 
+# CRTL + SHIFT + TAB
 bindkey '^[[Z' reverse-menu-complete
 
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
